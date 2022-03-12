7fcc353ae20385acdafc10c2011233f74168a152
Hardik Sachan <sachanhardik@gmail.com>
2022-03-12 11:01:24 +0530

    feat: implement find senders list in chat

--- analyzer.go
+++ analyzer.go
@@ -8,3 +8,14 @@ func CountMessagesOf(sender string, chat Chat) (count int) {
 	}
 	return
 }
+
+func ListAllSenders(chat Chat) (senders []string) {
+	senderSet := make(map[string]bool)
+	for _, message := range chat {
+		if _, ok := senderSet[message.Sender]; !ok {
+			senderSet[message.Sender] = true
+			senders = append(senders, message.Sender)
+		}
+	}
+	return
+}
--- analyzer_test.go
+++ analyzer_test.go
@@ -1,9 +1,48 @@
 package whatsappChatAnalyser_test
 
 import (
+	"fmt"
 	analyser "github.com/hardiksachan/whatsappChatAnalyser"
+	"reflect"
 	"testing"
 )
+
+func TestListAllSenders(t *testing.T) {
+	cases := []struct {
+		chat           analyser.Chat
+		expectedResult []string
+	}{
+		{analyser.Chat{
+			analyser.Message{Sender: "Nicole"},
+		}, []string{"Nicole"}},
+		{analyser.Chat{
+			analyser.Message{Sender: "Nicole"},
+			analyser.Message{Sender: "Nick"},
+		}, []string{"Nicole", "Nick"}},
+		{analyser.Chat{
+			analyser.Message{Sender: "Nicole"},
+			analyser.Message{Sender: "Nick"},
+			analyser.Message{Sender: "Nick"},
+		}, []string{"Nicole", "Nick"}},
+	}
+
+	for _, test := range cases {
+		t.Run(
+			fmt.Sprintf("list %d unique senders in %d messages", len(test.expectedResult), len(test.chat)),
+			func(t *testing.T) {
+				got := analyser.ListAllSenders(test.chat)
+
+				if !reflect.DeepEqual(got, test.expectedResult) {
+					t.Errorf(`
+want
+	%#v
+got
+	%#v`, test.expectedResult, got)
+				}
+			},
+		)
+	}
+}
 
 func TestCountMessageOfPerson(t *testing.T) {
 	cases := []struct {
